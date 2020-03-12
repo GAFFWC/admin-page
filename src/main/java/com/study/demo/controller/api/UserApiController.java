1468c2951363bd014352ae09790f837e444486b4
GAFFWC <41960405+GAFFWC@users.noreply.github.com>
2020-03-12 23:45:49 +0900

    User api 구현 - 1

--- src/main/java/com/study/demo/controller/api/UserApiController.java
+++ src/main/java/com/study/demo/controller/api/UserApiController.java
@@ -2,34 +2,36 @@ package com.study.demo.controller.api;
 
 import com.study.demo.ifs.CrudInterface;
 import com.study.demo.model.network.Header;
+import com.study.demo.model.network.request.UserApiRequest;
+import com.study.demo.model.network.response.UserApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
 
     @Override
     @PostMapping("") // api/user
-    public Header create() {
+    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
         return null;
     }
 
     @Override
     @GetMapping("{id}") // api/user/{id}
-    public Header read(@PathVariable(name = "id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("")
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody UserApiRequest userApiRequest) {
         return null;
     }
 
     @Override
     @DeleteMapping("{id}") // api/user/{id}
-    public Header delete(Long id) {
+    public Header delete(@PathVariable Long id) {
         return null;
     }
 }
--- src/main/java/com/study/demo/ifs/CrudInterface.java
+++ src/main/java/com/study/demo/ifs/CrudInterface.java
@@ -1,14 +1,16 @@
 package com.study.demo.ifs;
 
 import com.study.demo.model.network.Header;
+import com.study.demo.model.network.request.UserApiRequest;
+import com.study.demo.model.network.response.UserApiResponse;
 
-public interface CrudInterface {
+public interface CrudInterface<Req, Res> { // generic으로 수정하여 여러 api controller를 같은 형식으로 만들수 있도록
 
-    Header create(); // todo request object 추가
+    Header<Res> create(Req request); // todo request object 추가
 
-    Header read(Long id);
+    Header<Res> read(Long id);
 
-    Header update();
+    Header<Res> update(Req request);
 
     Header delete(Long id);
 }
